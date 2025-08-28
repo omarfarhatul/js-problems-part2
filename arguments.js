1ea4b83d37420b606e4b8ccd29eda30b27504dfa
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-28 16:44:23 +0600

    Add arguments

---
+++ arguments.js
@@ -0,0 +1,6 @@
+function add(n1, n2){
+    console.log(n1, n2);
+    console.log(arguments);
+}
+
+add(435, 75, 23, 1, 86, 43);
---
+++ array.js
@@ -0,0 +1,14 @@
+/* const products = [
+    {name: 'phone', price: 13454},
+    {name: 'laptop', price: 53535}
+]
+
+products[0];
+products[1]; */
+
+
+const products = {
+    '0': 15,
+    '1': 20,
+    '2': 56
+}
